a837375a1633a1e7f59bbef8f610c3b285415627
scofieldguo <scofied_1005@126.com>
2014-10-13 15:08:02 +0800

    Signed-off-by: scofieldguo <scofied_1005@126.com>

--- FirstGitProject/src/com/bw30/test/HelloWorld.java
+++ FirstGitProject/src/com/bw30/test/HelloWorld.java
@@ -3,6 +3,6 @@ package com.bw30.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("commin conflicated");
+		System.out.println("commin stash");
 	}
 }
